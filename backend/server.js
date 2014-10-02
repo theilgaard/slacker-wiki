9438fc1d79af00f3ce2bfa579a6fb8ff014e64b3
theilgaard <kristian.theilgaard@gmail.com>
2014-10-02 23:05:03 +0200

    Structure

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,21 @@ var express		= require('express');
 var app			= express();
 var bodyParser 	= require('body-parser');
 var mongoose 	= require('mongoose');
-var syscmds 	= require('./syscmds');
+var syscmds 	= require('./app/syscmds');
+
+// Models
+var page 		= require('./app/models/page');
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+var dbport = 27017;
 var port = process.env.PORT || 8080;
-
 var router = express.Router();
+
+// Mongo db 
+mongoose.connect('mongodb://86.52.78.30:27017/slacker-wiki');
 
 router.get('/', function(req, res){
 	res.json({ message: "root route! ah yah!"});
