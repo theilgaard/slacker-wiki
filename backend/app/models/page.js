a24185bb0534c4462bf74d8494769dbbc72cfc34
theilgaard <kristian.theilgaard@gmail.com>
2014-10-06 20:55:33 +0200

    CRUD Rest api on /api/pages

--- backend/app/models/page.js
+++ backend/app/models/page.js
@@ -2,7 +2,7 @@ var mongoose 	= require('mongoose');
 var Schema		= mongoose.Schema;
 
 var PageSchema	= new Schema({
-	name: String;
+	name: String
 });
 
 module.exports = mongoose.model('Page', PageSchema);
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ var mongoose 	= require('mongoose');
 var syscmds 	= require('./app/syscmds');
 
 // Models
-var page 		= require('./app/models/page');
+var Page 		= require('./app/models/page');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,16 +18,79 @@ var router = express.Router();
 // Mongo db 
 mongoose.connect('mongodb://86.52.78.30:27017/slacker-wiki');
 
+// Router middleware, handles all routes.
+router.use(function(req,res,next){
+	// do logging
+	console.log("something happened");
+	next();
+})
 router.get('/', function(req, res){
 	res.json({ message: "root route! ah yah!"});
 });
+// Do more routes...
 router.get('/sysinfo/cpu', function(req, res){
 	var cpustat = syscmds.getCpu(function(stdout){
 		//console.log("stdout was: "+stdout);
 		res.json({ cpu: stdout.replace(/\n$/,'') });
 	});
 });
-// Do more routes...
+router.route('/pages')
+	// Create a page on POST at /api/pages
+	.post(function(req, res){
+		var page = new Page();
+		page.name = req.body.name;
+
+		page.save(function(err){
+			if(err)
+				res.send(err);
+			res.json({message: 'Page created!'});
+		});
+	})
+	.get(function(req, res){
+		Page.find(function(err, pages){
+			if(err)
+				res.send(err);
+
+			res.json(pages);
+		});
+	});
+router.route('/pages/:page_id')
+	// Get a specific page
+	.get(function(req, res){
+		Page.findById(req.params.page_id, function(err, page){
+			if (err)
+				res.send(err);
+
+			res.json(page);
+		});
+	})
+	.put(function(req, res){
+		Page.findById(req.params.page_id, function(err, page){
+			if(err)
+				res.send(err);
+			page.name = req.body.name;
+
+			page.save(function(err){
+				if(err)
+					res.send(err);
+
+				res.json({message: 'Page updated!'});
+			});
+
+		});
+	})
+	.delete(function(req, res){
+		Page.remove({
+			_id: req.params.page_id
+		}, function(err, page){
+			if(err)
+				res.send(err);
+
+			res.json({message: 'Successfully deleted!'});
+		});
+	});
+
+
 
 app.use('/api', router);
 
